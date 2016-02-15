ae5f9e8c2b7e1a1cdc45b15cfb2b1e11d7474507
dwschrashun <dwschrashun@gmail.com>
2016-02-15 11:52:50 -0500

    fb app instantiating

--- client/pre-build/app.js
+++ client/pre-build/app.js
@@ -5,4 +5,28 @@ app.config(function ($urlRouterProvider, $locationProvider) {
    $locationProvider.html5Mode(true);
    // If we go to a URL that ui-router doesn't have registered, go to the "/" url.
    $urlRouterProvider.otherwise('/');
+
+   // Load the SDK asynchronously
+	(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js";
+	  fjs.parentNode.insertBefore(js, fjs);
+	  console.log("iife run");
+	}(document, 'script', 'facebook-jssdk'));
+});
+
+app.run(function ($rootScope) {
+	window.fbAsyncInit = function() {
+		FB.init({
+		    appId      : '829074960536435',
+		    cookie     : true,  // enable cookies to allow the server to access 
+		                        // the session
+		    xfbml      : true,  // parse social plugins on this page
+		    version    : 'v2.2' // use version 2.2
+		});
+		console.log("fbAsyncInit");
+		$rootScope.fbInit = true;
+	};
 });
--- client/pre-build/home/home.state.js
+++ client/pre-build/home/home.state.js
@@ -2,6 +2,26 @@ app.config(function($stateProvider) {
     $stateProvider.state('home', {
         url: '/',
         templateUrl: '/pre-build/home/home.html',
-        controller: 'HomeController'
+        controller: 'HomeController',
+        resolve: {
+	        checkInit: ['$rootScope', '$timeout', function ($rootScope, $timeout) {
+	        	console.log("resolving");
+	        	return new Promise(function(resolve) {
+	        		return resolve(waitForIt());
+	        	});
+
+	        	function waitForIt () {
+	        		return new Promise(function (resolve){
+		        		return resolve($timeout(function () {
+		        			console.log("timeout");
+			        		return new Promise(function(resolve){
+				        		if ($rootScope.fbInit) return resolve(true);
+				        		else return resolve(waitForIt());
+			        		});
+		        		}, 200));
+	        		});
+	        	}
+	        }]
+	    }
     });
 });
